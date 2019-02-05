e91892cba24154c1f29e50002b3dddf01c9b1fb2
Felipe Barros Pereira <felipe_bpereira@hotmail.com>
2019-02-04 22:45:35 -0200

    Estrutura TDD inicial, testes da classe MarsPlateau

---
+++ Nasa.SpaceProbe.Tests/MarsPlateauTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Nasa.SpaceProbe.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Xunit;
+
+    public class MarsPlateauTests
+    {
+        private readonly MarsPlateau;
+
+        [InlineData(5, 5)]
+        public void SetSize(int height, int width)
+        {
+            marsPlateau.SetSize(height, width);
+            Assert.Equals(marsPlateau.Size, new Size(height, width));
+        }
+    }
+}
